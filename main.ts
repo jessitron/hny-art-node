fa08b618e140487c380ef9001c004c12e353d40a
Jessica Kerr <jessitron@honeycomb.io>
2022-11-15 17:09:42 -0600

    Added tracing setup

--- main.ts
+++ main.ts
@@ -2,6 +2,21 @@ console.log("Greetings from Typescript");
 
 import { readImage } from "./image";
 
+// tracing.js
+'use strict';
+
+const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-proto");
+
+// The Trace Exporter exports the data to Honeycomb and uses
+// the environment variables for endpoint, service name, and API Key.
+const traceExporter = new OTLPTraceExporter();
+
+const sdk = new NodeSDK({
+  traceExporter
+});
+
+sdk.start()
 
 async function main() {
   const points = readImage();
