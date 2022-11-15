50e1b897fb4e040007b6776baec43c20f4e34c4c
Jessica Kerr <jessitron@honeycomb.io>
2022-11-15 14:52:53 -0600

    understand the ndarray returned by get-pixels

--- main.ts
+++ main.ts
@@ -3,14 +3,41 @@ console.log("Greetings from Typescript");
 import p from "get-pixels";
 import { promisify } from "util";
 
-async function main() {
-  const pixels = await promisify(p)("./hatted.png", "image/png");
+async function readPng(location: string) {
+  return promisify(p)(location, "image/png");
+}
 
-  console.log(JSON.stringify(pixels.shape.slice()));
+function range(from: number, to: number): ReadonlyArray<number> {
+  return [...Array(to - from).keys()].map((i) => i + from);
+}
+
+async function main() {
+  const pixels = await readPng("./hatskirt.png");
+  const width = pixels.shape[0];
+  const height = pixels.shape[1];
+
+  const alpha = pixels.pick(null, null, 4); // the fourth channel
+  const lines = range(0, height).map((y) =>
+    range(0, width)
+      .map((x) => alpha.get(x, y))
+      .map((n) => ("" + n).padStart(3, " "))
+      .join(" ")
+  );
+  lines.forEach((l) => console.log(l));
+  {
+    //blah
+    const x = 8;
+    const y = 1;
+    for (var ch = 1; ch <= 4; ch++) {
+      console.log(
+        `at ${x}, ${y}, ${ch}: ` + JSON.stringify(pixels.get(x, y, ch))
+      );
+    }
+  }
 }
 
 main();
 
-setTimeout(() => {
-  console.log("Good Bye");
-}, 10000);
+// setTimeout(() => {
+//   console.log("Good Bye");
+// }, 10000);
