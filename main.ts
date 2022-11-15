a179c398e0c4c8bb916b0e3277e3e90d09d0165f
Jessica Kerr <jessitron@honeycomb.io>
2022-11-15 14:27:07 -0600

    It does read an image

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
+console.log("Greetings from Typescript");
 
+import p from "get-pixels";
+import { promisify } from "util";
+
+async function main() {
+  const pixels = await promisify(p)("./hatted.png", "image/png");
+
+  console.log(JSON.stringify(pixels.shape.slice()));
+}
+
+main();
+
+setTimeout(() => {
+  console.log("Good Bye");
+}, 10000);
