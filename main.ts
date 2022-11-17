01015175986a44b5ee071129c4b1cebcc0af6c8e
Jessica Kerr <jessitron@honeycomb.io>
2022-11-17 11:00:48 -0600

    I can't figure out why it isn't doing the tracing
    We needed to wait for the SDK to start up ?!?
    
    still works

--- main.ts
+++ main.ts
@@ -6,26 +6,27 @@ import { readImage } from "./image";
 ("use strict");
 
 import { NodeSDK } from "@opentelemetry/sdk-node";
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 import {
   SimpleSpanProcessor,
   ConsoleSpanExporter,
   BatchSpanProcessor,
 } from "@opentelemetry/sdk-trace-base";
 
+const { DiagConsoleLogger, DiagLogLevel, diag } = require("@opentelemetry/api");
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+
 // The Trace Exporter exports the data to Honeycomb and uses
 // the environment variables for endpoint, service name, and API Key.
-const traceExporter = new OTLPTraceExporter();
+const traceExporter = new OTLPTraceExporter( );
 
 const sdk = new NodeSDK({
   spanProcessor: new BatchSpanProcessor(traceExporter, {
     scheduledDelayMillis: 500,
     maxQueueSize: 16000,
     maxExportBatchSize: 1000,
-  }),
+  })
 });
-
-sdk.start();
 
 import otel from "@opentelemetry/api";
 const tracer = otel.trace.getTracer("i did this on purpose");
@@ -37,6 +38,8 @@ type SpanSpec = {
 };
 
 async function main() {
+
+await sdk.start();
   const pixels = readImage();
   const allPixels = pixels.all();
 
