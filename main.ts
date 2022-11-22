534d54be2bdfb56011fa3a13446ef721453992af
Jessica Kerr <jessitron@gmail.com>
2022-11-22 18:11:10 +0000

    Remove trace initialization

--- main.ts
+++ main.ts
@@ -1,32 +1,9 @@
 console.log("Greetings from Typescript");
-
+import "./tracing"
 import { readImage } from "./image";
 
 // tracing.js
 ("use strict");
-
-import { NodeSDK } from "@opentelemetry/sdk-node";
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
-import {
-  SimpleSpanProcessor,
-  ConsoleSpanExporter,
-  BatchSpanProcessor,
-} from "@opentelemetry/sdk-trace-base";
-
-const { DiagConsoleLogger, DiagLogLevel, diag } = require("@opentelemetry/api");
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
-
-// The Trace Exporter exports the data to Honeycomb and uses
-// the environment variables for endpoint, service name, and API Key.
-const traceExporter = new OTLPTraceExporter();
-
-const sdk = new NodeSDK({
-  spanProcessor: new BatchSpanProcessor(traceExporter, {
-    scheduledDelayMillis: 500,
-    maxQueueSize: 16000,
-    maxExportBatchSize: 1000,
-  }),
-});
 
 import otel from "@opentelemetry/api";
 const tracer = otel.trace.getTracer("i did this on purpose");
@@ -38,7 +15,7 @@ type SpanSpec = {
 };
 
 async function main(imageFile: string) {
-  await sdk.start();
+  //await sdk.start();
   const pixels = readImage(imageFile);
   const allPixels = pixels.all().filter((p) => p.color.total() > 0);
 
