14193b8fd1857897ac7f5c003a59eb34a8079ad3
Jessica Kerr <jessitron@gmail.com>
2022-11-23 19:18:55 +0000

    Remove Honeycomb SDK

--- main.ts
+++ main.ts
@@ -2,31 +2,6 @@ console.log("Greetings from Typescript");
 import { sdk } from "./tracing"
 import { Darkness, Pixel, Pixels, readImage } from "./image";
 
-// tracing.js
-("use strict");
-
-import { NodeSDK } from "@opentelemetry/sdk-node";
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
-import {
-  SimpleSpanProcessor,
-  ConsoleSpanExporter,
-  BatchSpanProcessor,
-} from "@opentelemetry/sdk-trace-base";
-
-const { DiagConsoleLogger, DiagLogLevel, diag } = require("@opentelemetry/api");
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
-
-// The Trace Exporter exports the data to Honeycomb and uses
-// the environment variables for endpoint, service name, and API Key.
-const traceExporter = new OTLPTraceExporter();
-
-const sdk = new NodeSDK({
-  spanProcessor: new BatchSpanProcessor(traceExporter, {
-    scheduledDelayMillis: 500,
-    maxQueueSize: 16000,
-    maxExportBatchSize: 1000,
-  }),
-});
 
 import otel from "@opentelemetry/api";
 const tracer = otel.trace.getTracer("i did this on purpose");
@@ -37,15 +12,8 @@ type SpanSpec = {
   spans_at_once: number;
 };
 
-<<<<<<< HEAD
 type CountOfSpans = number; // 0 to maxSpansAtOnePoint
 function approximateColorByNumberOfSpans(allPixels: Pixel[]): (d: Darkness) => CountOfSpans {
-=======
-async function main(imageFile: string) {
-  await sdk.start();
-  const pixels = readImage(imageFile);
-  const allPixels = pixels.all().filter((p) => p.color.total() > 0);
->>>>>>> parent of 2e84291 (sdk)
 
   const bluenesses = allPixels.map((p) => p.color.darkness());
   const maxBlueness = Math.max(...bluenesses);
