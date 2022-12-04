f2669ccdbbef56e90bee2bbafc26de4f14ec4826
Jessica Kerr <jessitron@honeycomb.io>
2022-12-04 17:50:33 -0600

    Now it is all blue

---
+++ usefulTransformationOfTheMoment.ts
@@ -0,0 +1,15 @@
+import { readImage, Color } from "./image";
+
+const pixels = readImage("./santa7.png");
+
+const Purple = new Color(66, 19, 112, 255);
+
+const Transparent = new Color(0, 0, 0, 0);
+
+pixels.all().forEach((p) => {
+  if (p.color.darkness() > 0) {
+    pixels.overwrite(p.withColor(new Color(0, 0, p.color.blue, 255)));
+  }
+});
+
+pixels.writeToFile("santa8.png");
