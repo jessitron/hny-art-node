d0b0177928d10fdc286ea7aa1918c3b37e48566b
Jessica Kerr <jessitron@honeycomb.io>
2022-12-09 13:48:05 -0600

    Refactor, rearrange

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { sdk } from "./tracing";
-import { Darkness, Pixel, Pixels, readImage } from "./image";
+import { Pixels, readImage } from "./image";
 import { populateAttributes } from "./bubbleUp";
 
 import otel from "@opentelemetry/api";
@@ -11,69 +11,84 @@ import {
   SecondsSinceEpoch,
   HeatmapSpanSpec,
 } from "./heatmap";
-const tracer = otel.trace.getTracer("i did this on purpose");
+const tracer = otel.trace.getTracer("viz-art");
 
 console.log("Greetings! Here we go...");
 
 async function main(imageFile: string) {
   await sdk.start();
+
   const pixels = readImage(imageFile);
+  console.log(`Read ${pixels.width}x${pixels.height} image from ${imageFile}`);
+
+  const spanSpecs = planSpans(pixels);
+  console.log(`Preparing to send ${spanSpecs.length} spans...`);
+
+  const traceId = sendSpans(spanSpecs);
+  console.log("Trace ID is: " + traceId);
+
+  const link = await findLinkToDataset();
+  console.log("Run a new query for HEATMAP(height) in this dataset: " + link);
+
+  sdk.shutdown();
+  console.log("Pausing to send buffered spans...");
+  setTimeout(() => console.log("hopefully they've all been sent"), 20000);
+}
+
+type SpanSpec = HeatmapSpanSpec & Record<string, number | string>;
+
+function planSpans(pixels: Pixels): SpanSpec[] {
   const visiblePixels = pixels.all().filter((p) => p.color.darkness() > 0);
 
   const spansForColor = approximateColorByNumberOfSpans(visiblePixels);
   const heatmapHeight = placeVerticallyInBuckets(visiblePixels, pixels.height);
 
   // turn each pixel into some spans
-  const spanSpecs: HeatmapSpanSpec[] = visiblePixels
+  const spanSpecs = visiblePixels
     .map((p) => {
       const spans_at_once = spansForColor(p);
       return Array(spans_at_once)
         .fill(0)
         .map((_) => ({
           ...p.asFlatJson(), // add all the fields, for observability ;-)
-          ...populateAttributes(p),
+          spans_at_once,
           time_delta: p.location.x - pixels.width,
           height: heatmapHeight(p.location.y), // make it noninteger, so hny knows this is a float field
-          spans_at_once,
+          ...populateAttributes(p), // for bubble up
         }));
     })
     .flat();
 
-  console.log(`Preparing to send ${spanSpecs.length} spans...`);
+  return spanSpecs;
+}
 
+type TraceID = string;
+function sendSpans(spanSpecs: SpanSpec[]): TraceID {
   const begin: SecondsSinceEpoch = Date.now() / 1000;
-
+  var traceId: string;
   // the root span has no height, so it doesn't appear in the heatmap
-  tracer.startActiveSpan("Deck the halls with boughs of holly", (rootSpan) => {
-    // create all the spans for the picture
-    spanSpecs.sort(byTime).forEach((ss) => {
-      const s = tracer.startSpan("la", {
-        startTime: placeHorizontallyInBucket(begin, ss.time_delta),
-        attributes: ss,
+  return tracer.startActiveSpan(
+    "Deck the halls with boughs of holly",
+    (rootSpan) => {
+      // create all the spans for the picture
+      spanSpecs.sort(byTime).forEach((ss) => {
+        const s = tracer.startSpan("la", {
+          startTime: placeHorizontallyInBucket(begin, ss.time_delta),
+          attributes: ss,
+        });
+        s.end();
       });
-      s.end();
-    });
-    console.log("Trace ID is: " + rootSpan.spanContext().traceId);
-    rootSpan.end();
-  });
+      traceId = rootSpan.spanContext().traceId;
+      rootSpan.end();
+      return traceId;
+    }
+  );
 }
 
 const byTime = function (ss1: HeatmapSpanSpec, ss2: HeatmapSpanSpec) {
   return ss2.time_delta - ss1.time_delta;
 };
+
 const imageFile = process.argv[2] || "dontpeek.png";
 
-main(imageFile).then(async () => {
-  console.log("Pausing to send buffered spans...");
-
-  const link = await findLinkToDataset();
-  if (link) {
-    console.log("Run a new query for HEATMAP(height) in this dataset: " + link);
-  }
-
-  // TODO: print a link to the environment
-  // TODO: send them from the left rather than from the top
-  sdk.shutdown();
-
-  setTimeout(() => console.log("hopefully they've all been sent"), 20000);
-});
+main(imageFile);
