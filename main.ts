b01122d882e4ba073eadf6796e1f962e8636664f
Jessica Kerr <jessitron@honeycomb.io>
2022-12-09 13:25:25 -0600

    Refactor: move heatmap-specific functions out

--- main.ts
+++ main.ts
@@ -4,76 +4,16 @@ import { populateAttributes } from "./attributes";
 
 import otel from "@opentelemetry/api";
 import { findLinkToDataset } from "./honeyApi";
+import {
+  approximateColorByNumberOfSpans,
+  placeHorizontallyInBucket,
+  placeVerticallyInBuckets,
+  SecondsSinceEpoch,
+  SpanSpec,
+} from "./heatmap";
 const tracer = otel.trace.getTracer("i did this on purpose");
 
 console.log("Greetings! Here we go...");
-
-type CountOfSpans = number; // 0 to maxSpansAtOnePoint
-type SpanSpec = {
-  time_delta: number;
-  height: number;
-  spans_at_once: CountOfSpans;
-};
-
-type SecondsSinceEpoch = number;
-type Seconds = number;
-type Nanoseconds = number;
-type HrTime = [SecondsSinceEpoch, Nanoseconds];
-const Granularity: Seconds = 5;
-
-function approximateColorByNumberOfSpans(
-  allPixels: Pixel[]
-): (d: Pixel) => CountOfSpans {
-  const bluenesses = allPixels.map((p) => 255 - p.color.blue);
-  const maxBlueness = Math.max(...bluenesses);
-  const bluenessWidth = maxBlueness - Math.min(...bluenesses);
-  if (bluenessWidth === 0) {
-    // there is only one color. We only ever need to send 1 span.
-    return (d) => 1;
-  }
-  const maxSpansAtOnePoint = 10.0;
-  const increaseInSpansPerBlueness =
-    bluenessWidth === 0 ? 1 : (maxSpansAtOnePoint - 1) / bluenessWidth;
-  return (p: Pixel) =>
-    maxSpansAtOnePoint -
-    Math.round(
-      (maxBlueness - (255 - p.color.blue)) * increaseInSpansPerBlueness
-    );
-}
-
-type RowInPng = number; // distance from the top of the png, in pixels. Int
-type HeatmapHeight = number; // the height we should heatmap on. float. NEVER a whole number
-
-function placeVerticallyInBuckets(
-  visiblePixels: Pixel[],
-  imageHeight: number
-): (y: RowInPng) => HeatmapHeight {
-  const pictureHeight =
-    imageHeight - Math.min(...visiblePixels.map((p) => p.location.y));
-  const imageBase =
-    imageHeight - Math.max(...visiblePixels.map((p) => p.location.y));
-  const imageHeightRange = pictureHeight - imageBase + 1;
-  if (imageHeightRange > 50) {
-    console.log(
-      "WARNING: The picture is too tall. Make its content 25-50 pixels high"
-    );
-  }
-  if (imageHeightRange <= 25) {
-    console.log(
-      "WARNING: The picture is too short. Make its content 25-50 pixels high"
-    );
-  }
-  var predictedStepSize = 1.6777216; // this is just what it is. 0.0000001 * 2^24
-  return (y) =>
-    (imageHeight - y - imageBase + 0.5) * predictedStepSize + imageBase + 0.01;
-}
-
-function placeHorizontallyInBucket(
-  begin: SecondsSinceEpoch,
-  howFarToTheRight: number
-): HrTime {
-  return [begin + howFarToTheRight * Granularity, 0];
-}
 
 async function main(imageFile: string) {
   await sdk.start();
@@ -83,8 +23,10 @@ async function main(imageFile: string) {
   const spansForColor = approximateColorByNumberOfSpans(visiblePixels);
   const heatmapHeight = placeVerticallyInBuckets(visiblePixels, pixels.height);
 
+  type NewType = SpanSpec;
+
   // turn each pixel into some spans
-  const spanSpecs: SpanSpec[] = visiblePixels
+  const spanSpecs: NewType[] = visiblePixels
     .map((p) => {
       const spans_at_once = spansForColor(p);
       return Array(spans_at_once)
